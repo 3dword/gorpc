3133899890725f42e28b06eaa0ac6c2fbfa3bbbd
delvintang <delvintang@tencent.com>
2019-09-25 21:40:48 +0800

    modify server

--- client/client.go
+++ client/client.go
@@ -1,13 +1,37 @@
 package client
 
+import "time"
+
+// Client 定义了客户端通用接口
 type Client interface {
-	Dial(opt ...Options)
-	Recv()
+	Invoke(rspbody interface{}, opts ...Option)
 }
 
-type Options interface{
-	set(*Options)
+// Options 定义了客户端调用参数
+type Options struct {
+	// 调用地址
+	Target string
+	// 超时时间
+	Timeout time.Duration
+
 }
+
+func WithTarget(target string) Option {
+	return func(o *Options) {
+		o.Target = target
+	}
+}
+
+func WithTimeout(timeout time.Duration) Option {
+	return func(o *Options) {
+		o.Timeout = timeout
+	}
+}
+
+
+
+type Option func(*Options)
+
 
 type defaultClient struct {
 	options *Options
--- service.go
+++ service.go
@@ -1,8 +1,33 @@
 package gorpc
 
+import "context"
+
 // Service 定义了某个具体服务的通用实现接口
 type Service interface {
 	Register()
 	Serve()
 	Close()
 }
+
+
+type service struct{
+	serviceName string
+	handlers map[string]Handler
+}
+
+type Handler func(context.Context)
+
+
+func (s *service) Register(handlerName string, handler Handler) {
+	s.handlers[handlerName] = handler
+}
+
+func (s *service) Serve() {
+
+}
+
+
+func (s *service) Close() {
+
+}
+
