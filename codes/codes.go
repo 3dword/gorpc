50022d05fa9066a49c7980ee0c68cc531d842d54
delvintang <delvintang@tencent.com>
2019-09-29 21:46:35 +0800

    modify codes

--- codes/codes.go
+++ codes/codes.go
@@ -5,9 +5,44 @@ const (
 	OK = 0
 )
 
+type ErrorCode uint8
+
+// 错误码类型
+const (
+	FrameworkError = 1
+	BusinuessError = 2
+)
 
 // 框架错误码
 const (
 	CONFIGERROR = 201
 )
 
+
+// 业务错误码
+const (
+
+)
+
+type Error struct {
+	Code int
+	Type int32
+	Message string
+}
+
+const (
+	Success = "success"
+)
+
+func (e *Error) Error() string {
+	if e == nil {
+		return Success
+	}
+	if e.Type == FrameworkError {
+		return e.Message
+	}
+}
+
+func NewFrameError() {
+
+}
